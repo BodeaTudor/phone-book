e225d048a2fa35a29c0e37ea163c1e55deaeab38
Bodea Tudor <bodea.tudor99@yahoo.com>
2021-02-17 17:45:26 +0200

    *added a GET endpoint for retrieving items

--- src/main/java/org/example/web/PhoneBookItemServlet.java
+++ src/main/java/org/example/web/PhoneBookItemServlet.java
@@ -1,6 +1,7 @@
 package org.example.web;
 
 import org.example.config.ObjectMapperConfig;
+import org.example.domain.PhoneBookItem;
 import org.example.service.PhoneBookItemService;
 import org.example.transfer.CreateItemRequest;
 
@@ -11,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
 
 @WebServlet("/phone-book")
 public class PhoneBookItemServlet extends HttpServlet {
@@ -24,6 +26,21 @@ public class PhoneBookItemServlet extends HttpServlet {
 
         try {
             phoneBookItemService.createPhoneBookItem(request);
+        } catch (SQLException | ClassNotFoundException e) {
+            resp.sendError(500, "Internal server error: " + e.getMessage());
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            List<PhoneBookItem> phoneBookItems = phoneBookItemService.getPhoneBookItems();
+            String responseJson = ObjectMapperConfig.getObjectMapper().writeValueAsString(phoneBookItems);
+
+            resp.getWriter().print(responseJson);
+            resp.getWriter().flush();
+            resp.getWriter().close();
+
         } catch (SQLException | ClassNotFoundException e) {
             resp.sendError(500, "Internal server error: " + e.getMessage());
         }
