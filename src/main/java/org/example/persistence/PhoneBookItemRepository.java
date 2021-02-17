780db85ebbe06dcd9e39a1de12c4496dbcbf99a1
Bodea Tudor <bodea.tudor99@yahoo.com>
2021-02-17 17:05:53 +0200

    *added a method in repository for retrieving items from the database

--- src/main/java/org/example/persistence/PhoneBookItemRepository.java
+++ src/main/java/org/example/persistence/PhoneBookItemRepository.java
@@ -1,11 +1,12 @@
 package org.example.persistence;
 
 import org.example.config.DatabaseConfig;
+import org.example.domain.PhoneBookItem;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PhoneBookItemRepository {
 
@@ -22,5 +23,28 @@ public class PhoneBookItemRepository {
             preparedStatement.executeUpdate();
         }
 
+    }
+
+    public List<PhoneBookItem> getItems() throws SQLException, IOException, ClassNotFoundException {
+        String query = "SELECT id, first_name, last_name, phone_number FROM phone_book_items";
+
+        try (Connection connection = DatabaseConfig.getConnection();
+             Statement statement = connection.createStatement()
+        ) {
+            ResultSet resultSet = statement.executeQuery(query);
+
+            List<PhoneBookItem> phoneBookItems = new ArrayList<>();
+
+            while (resultSet.next()) {
+                PhoneBookItem item = new PhoneBookItem();
+                item.setId(resultSet.getLong("id"));
+                item.setFirstName(resultSet.getString("first_name"));
+                item.setLastName(resultSet.getString("last_name"));
+                item.setPhoneNumber(resultSet.getInt("phone_number"));
+
+                phoneBookItems.add(item);
+            }
+            return phoneBookItems;
+        }
     }
 }
